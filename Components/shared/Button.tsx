70231907fbe6e16ac5c0985f9276bed7a6f138b1
mxngls <Maximilian.e.hoenig@gmail.com>
2022-11-03 14:07:29 +0900

    Update CSS

--- Components/shared/Button.tsx
+++ Components/shared/Button.tsx
@@ -16,7 +16,7 @@ export default function Button({
         return (
             <button
                 type={type}
-                className="box-border h-11 w-fit rounded-md bg-slate-900 px-6 py-3 text-sm font-medium text-slate-50 shadow-sm hover:bg-slate-800"
+                className="box-border flex flex-nowrap items-center space-x-2 rounded-md bg-slate-800 px-3 py-2 text-center text-sm font-medium text-slate-50 shadow-sm hover:bg-slate-700"
                 onClick={onClickHandler}
             >
                 {children}
@@ -26,7 +26,7 @@ export default function Button({
         return (
             <button
                 type={type}
-                className="box-border h-11 w-fit rounded-md bg-slate-900 px-6 py-3 text-sm font-medium text-slate-50 shadow-sm hover:bg-slate-800"
+                className="box-border flex flex-nowrap items-center space-x-2 rounded-md bg-slate-800 px-3 py-2 text-center text-sm font-medium text-slate-50 shadow-sm hover:bg-slate-700"
             >
                 {children}
             </button>
