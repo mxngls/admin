033731754a3b716ebd1766c8daf7232d505d8aa2
mxngls <Maximilian.e.hoenig@gmail.com>
2022-11-03 14:28:02 +0900

    Format and add route to the list of applied routes

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,31 @@
-import { NextRequest, NextResponse, NextFetchEvent } from "next/server"
+import { NextRequest, NextResponse, NextFetchEvent } from "next/server";
 
 async function checkLogin(req: NextRequest) {
-    let token = req.cookies.get("sb-access-token")
+    let token = req.cookies.get("sb-access-token");
     if (!token) {
-        return null
+        return null;
     }
-    let authRequestResult = await fetch(`${process.env.NEXT_PUBLIC_SUPABASE_DB_URL}/auth/v1/user`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-            APIKey: process.env.NEXT_PUBLIC_SUPABASE_DB_PUBLIC_KEY!
-        },
-    })
-    return authRequestResult
+    let authRequestResult = await fetch(
+        `${process.env.NEXT_PUBLIC_SUPABASE_DB_URL}/auth/v1/user`,
+        {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                APIKey: process.env.NEXT_PUBLIC_SUPABASE_DB_PUBLIC_KEY!,
+            },
+        }
+    );
+    return authRequestResult;
 }
 
 export async function middleware(req: NextRequest, e: NextFetchEvent) {
-    const authRequestResult = await checkLogin(req)
+    const authRequestResult = await checkLogin(req);
     if (!authRequestResult || authRequestResult.status !== 200) {
-        return NextResponse.redirect(new URL("/admin/login", req.url))
+        return NextResponse.redirect(new URL("/admin/login", req.url));
     } else {
-        return NextResponse.next()
+        return NextResponse.next();
     }
 }
 
 export const config = {
-    matcher: "/admin/product/:path*",
-}
+    matcher: ["/admin/product/:path*", "/adming/products"],
+};
