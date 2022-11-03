e125365892a37ba939fa17e91be7e8a0437e5d2b
mxngls <Maximilian.e.hoenig@gmail.com>
2022-11-03 14:11:57 +0900

    Add new minimum screen size "xs"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    theme: {
+        screens: {
+            // Add smaller scrensize for image grid 
+            // (components/admin/Product/ProductImageContainer)
+            "xs": "520px",
+            ...defaultTheme.screens,
+        },
+    },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    plugins: [],
+};
