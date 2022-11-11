689aa7d5468c33618785383557e83eb8f6ce4c2c
mxngls <Maximilian.e.hoenig@gmail.com>
2022-11-11 16:37:01 +0900

    Minor CSS changes

--- Components/admin/ProductTable/OptionsContainer/SortPopupContainer.tsx
+++ Components/admin/ProductTable/OptionsContainer/SortPopupContainer.tsx
@@ -33,46 +33,50 @@ export default function SortPopupContainer({
         <div
             className={`absolute mt-2 flex-row items-center overflow-visible whitespace-nowrap rounded border-[1px] border-slate-200 bg-slate-50 px-2`}
         >
-            <div>
-                <div className="flex min-h-[3rem] min-w-full flex-col items-center justify-center border-b-[1px] border-slate-200 py-1">
-                    {sortRules.length === 0 ? (
-                        <span className="ml-2 place-self-center self-start  text-left">
-                            No rules applied
-                        </span>
-                    ) : (
-                        sortRules.map((rule, index) => {
-                            return (
-                                <ActiveRule
-                                    key={rule.column}
-                                    rule={rule}
-                                    index={index}
-                                    sortRules={sortRules}
-                                    setSortRules={setSortRules}
-                                    setSortPopup={setSortPopup}
-                                />
-                            );
-                        })
+            <div className="justify-center-1 flex min-h-[3rem] min-w-full flex-col items-center justify-center">
+                {sortRules.length === 0 ? (
+                    <span className="mx-2 place-self-center self-start  text-left">
+                        No rules applied
+                    </span>
+                ) : (
+                    sortRules.map((rule, index) => {
+                        return (
+                            <ActiveRule
+                                key={rule.column}
+                                rule={rule}
+                                index={index}
+                                sortRules={sortRules}
+                                setSortRules={setSortRules}
+                                setSortPopup={setSortPopup}
+                            />
+                        );
+                    })
+                )}
+            </div>
+            <div className="mr-1 min-h-[2.5rem] w-full">
+                <div className="relative">
+                    <div className="border-t-[1px] border-slate-200 py-3">
+                        <button
+                            className="flex items-center rounded py-1 px-2 text-sm hover:bg-slate-200"
+                            onClick={() =>
+                                setShowColumnList((current) => !current)
+                            }
+                        >
+                            <span>Pick a Column to Sort by</span>
+                            <NavArrowDown className="ml-1" />
+                        </button>
+                    </div>
+                    {!!showColumnList && (
+                        <div className="flex-col border-t-[1px] border-slate-200 py-1">
+                            <ColumnList
+                                columnsData={columnsData}
+                                setShowColumnList={setShowColumnList}
+                                current={sortRules}
+                                onClickHandler={addRule}
+                            />{" "}
+                        </div>
                     )}
                 </div>
-            </div>
-            <div className="mr-1 flex min-h-[2.5rem] min-w-full flex-col">
-                <div className="border-b-[1px] border-slate-200 py-2">
-                    <button
-                        className="flex items-center rounded py-1 px-2 text-sm hover:bg-slate-200"
-                        onClick={() => setShowColumnList((current) => !current)}
-                    >
-                        <span>Pick a Column to Sort by</span>
-                        <NavArrowDown className="ml-1" />
-                    </button>
-                </div>
-                {!!showColumnList && (
-                    <ColumnList
-                                columnsData={columnsData}
-                        setShowColumnList={setShowColumnList}
-                                current={sortRules}
-                        onClickHandler={addRule}
-                            />{" "}
-                )}
             </div>
         </div>
     );
