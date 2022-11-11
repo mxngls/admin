97e1c34424bd499b418b6c0c69290bfa64e8c90f
mxngls <Maximilian.e.hoenig@gmail.com>
2022-11-11 16:19:48 +0900

    Make background cover green on hover when active

--- Components/admin/ProductTable/OptionsContainer/SortButton.tsx
+++ Components/admin/ProductTable/OptionsContainer/SortButton.tsx
@@ -19,11 +19,11 @@ export default function SortButton({
                 setSortPopup((current) => !current);
             }}
             className={`${
-                rulesNumber > 1 ? "hover:bg-green-50" : "hover:bg-slate-50"
+                rulesNumber > 0 ? "hover:bg-green-50" : "hover:bg-slate-50"
             } flex flex-row items-center space-x-2 rounded px-3 py-2`}
         >
-            <Sort color={`${rulesNumber > 1 ? "rgb(34 197 94" : ""}`} />
-            {rulesNumber > 1 ? (
+            <Sort color={`${rulesNumber > 0 ? "rgb(34 197 94" : ""}`} />
+            {rulesNumber > 0 ? (
                 <span className="hidden w-12 font-medium text-green-500 sm:block">
                     Sorted
                 </span>
