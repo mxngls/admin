2274da5bf211c4e303b16657572824e1ceabecf5
mxngls <Maximilian.e.hoenig@gmail.com>
2022-11-24 15:58:41 +0900

    Add custom screen size
    Add another custom screen size to make the table header stick correctly right under the table options even if these resize.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,17 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
     theme: {
         screens: {
-            // Add smaller scrensize for image grid 
+            // Add smaller scrensize for image grid
             // (components/admin/Product/ProductImageContainer)
-            "xs": "520px",
+            xxs: "392px",
+            xs: "520px",
             ...defaultTheme.screens,
+        },
+        extend: {
+            screens: {
+                md: "728px",
+                // => @media (min-width: 992px) { ... }
+            },
         },
     },
     content: [
