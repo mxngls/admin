a9ba9bec97b74ec9fb9d3031145239fd2d10b304
mxngls <Maximilian.e.hoenig@gmail.com>
2022-12-18 16:35:48 +0900

    Add debounce function and remove unused imports

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,31 +1,46 @@
-import { useEffect, useRef, useState } from "react";
-import { InputError } from "./types";
-import { ImageData } from "./types";
-
 const capitalizeFirstLetter = (string: string, locale = navigator.language) => {
-    const first = string.substring(0, 1);
-    const rest = string.substring(1);
-    if (first === undefined) {
-        return "";
-    } else {
-        return first.toLocaleUpperCase(locale) + rest.toLocaleLowerCase();
-    }
+  const first = string.substring(0, 1);
+  const rest = string.substring(1);
+  if (first === undefined) {
+    return "";
+  } else {
+    return first.toLocaleUpperCase(locale) + rest.toLocaleLowerCase();
+  }
 };
 
 const formatValue = (attribute: string, value: string | number) => {
-    if (attribute === "price") {
-        return new Intl.NumberFormat("ko-KR", {
-            style: "currency",
-            currency: "KRW",
-        }).format(value as number);
-    }
-    return value;
+  if (attribute === "price") {
+    return new Intl.NumberFormat("ko-KR", {
+      style: "currency",
+      currency: "KRW",
+    }).format(value as number);
+  }
+  return value;
 };
 
 const convertNumberType = (type: string) => {
-    // Convert types returned from Postgres query to JS Objects
-    // to enable comparision in useProductAttributeError hook
-    return type === "string" ? "string" : "number";
+  // Convert types returned from Postgres query to JS Objects
+  // to enable comparision in useProductAttributeError hook
+  return type === "string" ? "string" : "number";
 };
 
-export { capitalizeFirstLetter, formatValue, convertNumberType };
+// Corresponds to 10 frames at 60 Hz.
+// A few bytes payload overhead when lodash/debounce is ~3 kB and debounce ~300 B.
+const debounce = (callback: Function, wait = 166) => {
+  let timeout: NodeJS.Timeout;
+  const debounced = (...args: any[]) => {
+    const later = () => {
+      callback.apply(args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+
+  debounced.clear = () => {
+    clearTimeout(timeout);
+  };
+
+  return debounced;
+};
+
+export { capitalizeFirstLetter, formatValue, convertNumberType, debounce };
