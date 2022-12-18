7a6f66eab76037db80772fe2f3a9d8245bee4ed8
mxngls <Maximilian.e.hoenig@gmail.com>
2022-12-18 17:32:32 +0900

    Fix jumping when input error present

--- Components/admin/Product/ProductTitle.tsx
+++ Components/admin/Product/ProductTitle.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { capitalizeFirstLetter } from "../../../lib/helpers";
 import { useClickOutside, useProductAttributeError } from "../../../lib/hooks";
@@ -18,9 +18,7 @@ export default function ProductTitle({
   type,
   setProduct,
 }: ProductTitleAttributes) {
-  const [isEdit, setIsEdit] = useState<Boolean>(false);
   const [content, setContent] = useState<string | number>(name);
-  const formRef = useRef<HTMLFormElement>(null);
 
   useEffect(() => {
     setContent(name);
@@ -29,19 +27,8 @@ export default function ProductTitle({
   let err = useProductAttributeError(content, type);
 
   const containerRef: React.RefObject<HTMLDivElement> = useClickOutside(() => {
-    setIsEdit(false);
     setContent(name);
   });
-
-  const handleOnClick = () => {
-    const selection = window.getSelection();
-    if (selection?.type !== "Range")
-      if (!!isEdit) {
-        setIsEdit(false);
-      } else {
-        setIsEdit(true);
-      }
-  };
 
   const handleChangeContent = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (Number(event.target.value)) {
@@ -68,42 +55,28 @@ export default function ProductTitle({
     }
   };
 
-  if (isEdit) {
-    return (
-      <div ref={containerRef}>
-        <form className="" onSubmit={handleSubmit} ref={formRef}>
-          <input
-            type={"text"}
-            required={true}
-            value={content}
-            autoFocus={true}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-              handleChangeContent(event)
-            }
-            spellCheck={false}
-            className={`box-border h-[78px] w-full border-b-2 border-solid border-slate-200 pb-[18px] text-6xl font-semibold text-slate-700 outline-none invalid:border-pink-500 invalid:text-pink-600 focus:pb-4 focus:invalid:border-pink-500                         
+  return (
+    <div ref={containerRef}>
+      <form onSubmit={handleSubmit}>
+        <input
+          type={"text"}
+          required={true}
+          value={content}
+          autoFocus={true}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleChangeContent(event)
+          }
+          spellCheck={false}
+          className={`mb-8 box-border h-[78px] w-full border-b-2 border-solid border-slate-200 text-6xl font-semibold text-slate-700 outline-none invalid:border-pink-500 invalid:text-pink-600 focus:pb-4 focus:invalid:border-pink-500
                         ${
                           err.isErr &&
-                          "border-pink-500 text-pink-600 focus:border-b-2 focus:border-pink-500"
+                          "mb-0 border-pink-500 text-pink-600 focus:border-b-2 focus:border-pink-500"
                         } `}
-          ></input>
-          {err.isErr && <span className="text-pink-600">{err.message}</span>}
-        </form>
-      </div>
-    );
-  } else
-    return (
-      <div ref={containerRef}>
-        <h1
-          className="mb-2 box-border h-[78px] w-full border-b-2 border-solid border-slate-200 pb-[18px] text-6xl text-slate-400 transition-all duration-75 ease-in"
-          onClick={handleOnClick}
-        >
-          {!!name ? (
-            name
-          ) : (
-            <span className="text-slate-400 ">{"insert a name"}</span>
-          )}
-        </h1>
-      </div>
-    );
+        />
+        {err.isErr && (
+          <div className="my-3 h-5 text-pink-600">{err.message}</div>
+        )}
+      </form>
+    </div>
+  );
 }
